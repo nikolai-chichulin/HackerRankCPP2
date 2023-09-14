486e36534729c5b4e4e0589404c8f93d5c8113ca
Николай <nikolai.chichulin@gmail.com>
2023-09-14 20:06:41 +0300

    Final version of Euler39.

--- Euler39/Solution39.cpp
+++ Euler39/Solution39.cpp
@@ -29,7 +29,7 @@ void print_array(ostream& ostr, const int* v, int k) {
 /// Constructs the ntriples array.
 /// </summary>
 /// <param name="pmax">The maximal acceptable perimeter.</param>
-void solve_ab(li pmax) {
+void solve_ab_v0(li pmax) {
     int n = 0;
     li amax = 2500; // li(sqrt(pmax)) + 1;
     for (li a = 3; a < amax; a += 2) {
@@ -40,15 +40,65 @@ void solve_ab(li pmax) {
             li y = (a_2 - b_2) / 2;
             li z = (a_2 + b_2) / 2;
             li p = x + y + z;
-            if (p <= pmax) {
-                // p is the perimeter of the primitive triple, we need all the multiplications up to pmax
-                li ntriplesp = pmax / p;
-                n++;
-                //outf << n << ": a = " << a << " b = " << b << " x = " << x << " y = " << y << " z = " << z << " P = " << p << " N = " << ntriplesp << endl;
-                for (li it = 1; it <= ntriplesp; it++) {
-                    ntriples[p * it]++;
-                }
+            //outf << n << ": a = " << a << " b = " << b << " x = " << x << " y = " << y << " z = " << z << " P = " << p << " N = " << ntriplesp << endl;
+            // p is the perimeter of the primitive triple, we need all the multiplications up to pmax
+            li pp = p;
+            while (pp <= pmax) {
+                ntriples[pp]++;
+                pp += p;
             }
+        }
+    }
+}
+
+li getgcd(li d1, li d2) {
+    li a = d1 > d2 ? d1 : d2; // max
+    li b = (a == d1) ? d2 : d1; // min
+    while (b != 0) {
+        li t = b;
+        b = a % b;
+        a = t;
+    }
+    return a;
+}
+
+bool arevalid(li a, li b) {
+    if (a % 2 != 0 && b % 2 != 0) { // not both odd
+        return false;
+    }
+    li gcd = getgcd(a, b);
+    if (gcd > 1) {
+        return false;
+    }
+    return true;
+}
+
+/// <summary>
+/// Constructs the ntriples array.
+/// </summary>
+/// <param name="pmax">The maximal acceptable perimeter.</param>
+void solve_ab_v1(li pmax) {
+    int n = 0;
+    li amax = 2500; // li(sqrt(pmax)) + 1;
+    for (li a = 1; a < amax; a++) {
+        li a_2 = a * a;
+        for (li b = 1; b < a; b++) {
+            if (!arevalid(a, b)) {
+                continue;
+            }
+            n++;
+            li b_2 = b * b;
+            li x = 2 * a * b;
+            li y = (a_2 - b_2);
+            li z = (a_2 + b_2);
+            li p = x + y + z;
+            // p is the perimeter of the primitive triple, we need all the multiplications up to pmax
+            li pp = p;
+            while (pp <= pmax) {
+                ntriples[pp]++;
+                pp += p;
+            }
+            outf << n << ": a = " << a << " b = " << b << " x = " << x << " y = " << y << " z = " << z << " P = " << p << endl;
         }
     }
 }
@@ -74,7 +124,7 @@ void solve() {
     outf.open("euler39.out");
     auto start = std::chrono::high_resolution_clock::now();
 
-    solve_ab(dim - 1);
+    solve_ab_v1(dim - 1);
     finalize(dim - 1);
     cout << maxtriples_perimeter[5000000] << endl;
 
