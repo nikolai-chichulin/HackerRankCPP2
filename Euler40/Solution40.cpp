535898ccac7a6a4729c6606e2c7399b87056c93d
Николай <nikolai.chichulin@gmail.com>
2023-09-15 16:40:24 +0300

    Final version of the Euler40.

--- Euler40/Solution40.cpp
+++ Euler40/Solution40.cpp
@@ -11,11 +11,11 @@ using namespace std;
 
 typedef long long li; // long integer
 
-ofstream outf;
-
 const li dim = 17l;
 li sk[dim] = {};
-li decimals[] = { 1l,10l,100l,1000l,10000l,100000l,1000000l,10000000l,100000000l,1000000000l,10000000000l,100000000000l,1000000000000l,10000000000000l,100000000000000l,1000000000000000l,10000000000000000l,100000000000000000l,1000000000000000000l };
+li powers_of_ten[] = { 1l,10l,100l,1000l,10000l,100000l,1000000l,10000000l,
+100000000l,1000000000l,10000000000l,100000000000l,1000000000000l,10000000000000l,
+100000000000000l,1000000000000000l,10000000000000000l,100000000000000000l,1000000000000000000l };
 
 /// <summary>
 /// Construct the array Sk = 9 + 2*90 + 3*900 + ... + k*10^k
@@ -44,20 +44,70 @@ li getk(li i) {
     return k;
 }
 
-void solve(li i) {
+/// <summary>
+/// Returns a digit which is placed at the given position in the given number.
+/// </summary>
+/// <param name="n">The number.</param>
+/// <param name="order">Decimal order of the number.</param>
+/// <param name="position">1-based position started from the begining (highest order).</param>
+/// <returns></returns>
+li getdigit(li n, li order, li position) {
+
+
+    //li m = n;
+    //while (m != 0) {
+    //    li rem = m % 10;
+    //    ret.push_back(rem);
+    //    m /= 10;
+    //}
+    //return ret;
+
+
+    li left = n % powers_of_ten[order + 2 - position];
+    li right = powers_of_ten[order + 1 - position];
+    li ret = left / right;
+    return ret;
+}
+
+li solve(li i) {
     li k = getk(i); // find the range
     li is = k == 0 ? i : i - sk[k - 1]; // 1-based position inside the k-th range
     li iss = is % (k + 1) == 0 ? is / (k + 1) : is / (k + 1) + 1; // 1-based number of the k-digit number in the range
-    li number = decimals[k] + iss - 1;
-    cout << "i = " << i << " k = " << k << " is = " << is << " iss = " << iss << " number = " << number << endl;
+    li number = powers_of_ten[k] + iss - 1; // the number containing the given digit i
+    li isss = is - (k + 1) * (iss - 1); // 1-based position of the i inside the number
+    li digit = getdigit(number, k, isss);
+    cout << "i = " << i << " k = " << k << " is = " << is << " iss = " << iss << " number = " << number << " isss = " << isss << " digit = " << digit << endl;
+    return digit;
 }
 
 int main() {
 
     makesk();
 
-    li i = 193l; // 1-based position of the digit started from the left
-    solve(i);
+    li res = 1;
+    li i = 1111101l; // 1-based position of the digit started from the left
+    li d = solve(i);
+    res *= d;
+    i = 2111102l; // 1-based position of the digit started from the left
+    d = solve(i);
+    res *= d;
+    i = 3111103l; // 1-based position of the digit started from the left
+    d = solve(i);
+    res *= d;
+    i = 4111104l; // 1-based position of the digit started from the left
+    d = solve(i);
+    res *= d;
+    i = 5111105l; // 1-based position of the digit started from the left
+    d = solve(i);
+    res *= d;
+    i = 6111106l; // 1-based position of the digit started from the left
+    d = solve(i);
+    res *= d;
+    i = 7111107l; // 1-based position of the digit started from the left
+    d = solve(i);
+    res *= d;
+
+    cout << "Answer = " << res << endl;
 
     return 0;
 }
