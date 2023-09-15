9d1867c5f4e35ed38d81ddc31fcb7706aa108f44
Николай <nikolai.chichulin@gmail.com>
2023-09-15 14:52:59 +0300

    Updated the Euler40.

--- Euler40/Solution40.cpp
+++ Euler40/Solution40.cpp
@@ -15,6 +15,7 @@ ofstream outf;
 
 const li dim = 17l;
 li sk[dim] = {};
+li decimals[] = { 1l,10l,100l,1000l,10000l,100000l,1000000l,10000000l,100000000l,1000000000l,10000000000l,100000000000l,1000000000000l,10000000000000l,100000000000000l,1000000000000000l,10000000000000000l,100000000000000000l,1000000000000000000l };
 
 /// <summary>
 /// Construct the array Sk = 9 + 2*90 + 3*900 + ... + k*10^k
@@ -43,13 +44,20 @@ li getk(li i) {
     return k;
 }
 
+void solve(li i) {
+    li k = getk(i); // find the range
+    li is = k == 0 ? i : i - sk[k - 1]; // 1-based position inside the k-th range
+    li iss = is % (k + 1) == 0 ? is / (k + 1) : is / (k + 1) + 1; // 1-based number of the k-digit number in the range
+    li number = decimals[k] + iss - 1;
+    cout << "i = " << i << " k = " << k << " is = " << is << " iss = " << iss << " number = " << number << endl;
+}
+
 int main() {
 
     makesk();
 
-    li i = 1l;
-    li k = getk(i);
-    cout << "i = " << i << " k = " << k << endl;
+    li i = 193l; // 1-based position of the digit started from the left
+    solve(i);
 
     return 0;
 }
