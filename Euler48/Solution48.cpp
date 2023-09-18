77e5edc20f9f6804227c2245b3444b280401920e
Николай <nikolai.chichulin@gmail.com>
2023-09-18 17:15:32 +0300

    Updated the Euler48.

--- Euler48/Solution48.cpp
+++ Euler48/Solution48.cpp
@@ -4,28 +4,34 @@
 #include <iostream>
 #include <algorithm>
 #include <chrono>
+#include <fstream>
 
 using namespace std;
 
+ofstream outf;
+
 const int dim = 2000001;
 const int dim_primes = 150000;
-typedef unsigned long long int li;
-const li tol = 10000000000l;
-const li maxlisqr = li(sqrt(LLONG_MAX));
+typedef unsigned long long int ul;
+const ul tol = 10000000000l;
+const ul maxlisqr = ul(sqrt(LLONG_MAX));
 
-li s[dim]; // the terms: S = s1 + s2 + s3 + ... + sN, where s2 = 2^2, s30 = 30^30, sN = N^N
-li primes[dim_primes]; // primes <= 2 000 000
+ul s[dim]; // the terms: S = s1 + s2 + s3 + ... + sN, where s2 = 2^2, s30 = 30^30, sN = N^N
+ul primes[dim_primes]; // primes <= 2 000 000
 bool isprime[dim];
 
+const ul edge = 4000000000l;
+ul memo[200000000] = {};
+
 void getprimes() {
-    for (li i = 2; i < dim; i++) {
+    for (ul i = 2; i < dim; i++) {
         isprime[i] = true;
     }
     // cout << " ---//--- " << endl;
-    for (li i = 2; i < dim; i++) {
+    for (ul i = 2; i < dim; i++) {
         if (isprime[i]) {
-            li jst = i * i;
-            for (li j = jst; j < dim; j += i) {
+            ul jst = i * i;
+            for (ul j = jst; j < dim; j += i) {
                 isprime[j] = false;
             }
         }
@@ -33,11 +39,161 @@ void getprimes() {
     }
 }
 
-li power(li base, li exp) {
-    li ret = 1;
+bool is_small(ul n) {
+    return n <= edge;
+}
+
+bool is_small(ul n1, ul n2) {
+    return (n1 / 10 + 1) * (n2 / 10 + 1) < LLONG_MAX / 100;
+}
+
+ul safe_mod_multiplication(ul a, ul b, ul m) {
+    ul ret = 0;
+    if (is_small(a, b)) {
+        ret = (a * b) % m;
+    }
+    else {
+        ul mx = a > b ? a : b; // maximum
+        ul mn = mx == a ? b : a; // minimum
+        if (mx % 2l == 0) {
+            ul mx_2 = mx / 2l;
+            ret = safe_mod_multiplication(safe_mod_multiplication(mx_2, mn, m), 2l, m);
+        }
+        else {
+            ul mx_2 = (mx - 1l) / 2l;
+            ret = safe_mod_multiplication(safe_mod_multiplication(mx_2, mn, m), 2l, m);
+            ret = (ret + mn) % m;
+        }
+    }
+    //if (a == b)
+    //    outf << "Debug: a = " << a << " b = " << b << " ret = " << ret << endl;
+    return ret;
+}
+
+void safe_mod_multiplication_test() {
+    bool passed = true;
+
+    ul n1 = 4836715601l;
+    ul n2 = 2569995622l;
+    ul res_exp = 9429098822l;
+    ul res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 3415897173l;
+    n2 = 5114489489l;
+    res_exp = 6813314597l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 654654654656l;
+    n2 = 54654354768l;
+    res_exp = 1546999808l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 4294967296l;
+    n2 = 2147483648l;
+    res_exp = 6854775808l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 8642381783621l;
+    n2 = 45111235899977l;
+    res_exp = 2412876717l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 1l;
+    n2 = 1l;
+    res_exp = 1l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 1l;
+    n2 = 2l;
+    res_exp = 2l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 2l;
+    n2 = 2l;
+    res_exp = 4l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    n1 = 11l;
+    n2 = 22l;
+    res_exp = 242l;
+    res = safe_mod_multiplication(n1, n2, tol);
+    if (res != res_exp) {
+        cout << n1 << " * " << n2 << " = " << res_exp << " actual = " << res << " failed!" << endl;
+        passed = false;
+    }
+    else {
+        //cout << n1 << " * " << n2 << " = " << res << " passed!" << endl;
+    }
+
+    if (passed) {
+        cout << "Passed!" << endl;
+    }
+    else {
+        cout << "Failed!" << endl;
+    }
+}
+
+ul mod_power(ul base, ul exp, ul m) {
+    ul ret = 1;
     for (int i = 0; i < exp; i++) {
         ret *= base;
-        ret %= tol;
+        ret %= m;
         if (ret == 0) {
             break;
         }
@@ -45,80 +201,68 @@ li power(li base, li exp) {
     return ret;
 }
 
-li binpow(li a, li b, li m) {
-    a %= m;
-    li res = 1;
+ul mod_binpow(ul a, ul b, ul m) {
+    //cout << "Debug: a = " << a << " b = " << b;
+    ul ret = 1;
     while (b > 0) {
         if (b & 1)
-            res = (res * a) % m;
-        a = (a * a) % m;
+            ret = safe_mod_multiplication(ret, a, m); // (res * a) % m;
+        a = safe_mod_multiplication(a, a, m); // (a * a) % m;
         b >>= 1;
     }
-    return res;
-}
-
-li binpowrec(li a, li b, li m) {
-    if (b == 0) {
-        return 1l;
-    }
-    li c = binpowrec(a, b / 2, m);
-    if (b % 2 == 0) {
-        if (c > maxlisqr) {
-            cout << " danger c for a = " << a << " ";
-        }
-        c *= c;
-        return c % m;
-    }
-    c *= c;
-    return a * c % m;
-}
-
-void solveBF(li n) {
-    li ret = 0;
-    for (li i = 1; i < n + 1; i++) {
-        li t = power(i, i);
-        ret += t;
-        ret %= tol;
-        if (i % 100 == 0)
-            cout << "i = " << i << " : " << ret << endl;
-    }
-    cout << "n = " << n << " : " << ret << endl;
+    //cout << " ret = " << ret << endl;
+    return ret;
 }
 
 void test() {
-    for (li a = 2l; a < 100l; a++) {
-        li c1 = power(a, a);
-        li c2 = binpow(a, a, tol);
-        li c3 = binpowrec(a, a, tol);
-        cout << " a = " << a << " : res1 = " << c1 << " res2 = " << c2 << " res3 = " << c3 << endl;
+    ul N = 200000l;
+    ul s1 = 0;
+    ul s2 = 0;
+    for (ul a = 1l; a <= N; a++) {
+        //ul c1 = mod_power(a, a, tol);
+        ul c2 = mod_binpow(a, a, tol);
+        //if (c1 != c2) {
+        //    cout << " Failed for a = " << a << "!!!" << endl;
+        //}
+        //s1 += c1;
+        //s1 %= tol;
+        s2 = (s2 + c2) % tol;
+
+        //if (a % 100000 == 0)
+        //    cout << a << " done" << endl;
+    }
+    cout << "Sum1 = " << s1 << " Sum2 = " << s2 << endl;
+}
+
+void test_one() {
+    ul a = 1999999l;
+    ul b = 1999999l;
+    ul c1 = mod_power(a, b, tol);
+    cout << "c1 = " << c1 << endl;
+    ul c2 = mod_binpow(a, b, tol);
+    cout << "c2 = " << c2 << endl;
+
+    if (c1 != c2) {
+        cout << " Failed!!!" << endl;
+    }
+    else {
+        cout << " Passed!!!" << endl;
     }
 }
 
-int findOdd(int arr[], int n)
-{
-    int res = 0, i;
-    for (i = 0; i < n; i++)
-        res ^= arr[i];
-    return res;
-}
-
-// Driver Method
-int solve1()
-{
-    int arr[] = { 12, 12, 14, 90, 14, 14, 14 };
-    int n = sizeof(arr) / sizeof(arr[0]);
-    printf("The odd occurring element is %d ",
-        findOdd(arr, n));
-    return 0;
-}
-
 int main() {
+
+    outf.open("Euler48.out");
 
     /* Enter your code here. Read input from STDIN. Print output to STDOUT */
     auto start = std::chrono::high_resolution_clock::now();
 
     //solveBF(10000);
     test();
+    //test_one();
+    //safe_mod_multiplication_test();
+
+    outf.close();
 
     auto stop = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::microseconds>(stop - start);
